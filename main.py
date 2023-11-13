a84893bba4d37abc3de0950c0b01ca8c887b0110
citomirella <150628385+citomirella@users.noreply.github.com>
2023-11-13 17:49:51 +0000

    Nome, idade, peso e altura

--- main.py
+++ main.py
@@ -23,7 +23,12 @@ print(2 * '\n')
 print("-------------------------")
 print(2 * '\n')
 
+
+
 nome1 = input("Digite o seu nome: ")
 idade1 = int(input("Digite a sua idade: "))
 altura1 = float(input("Digite a sua altura: "))
 print("Seu nome é", nome1, ", você tem", idade1, "anos e", altura1, "de altura.")
+
+
+
