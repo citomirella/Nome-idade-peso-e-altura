73d0036af2c6d6b8438151f548d15408031f0456
citomirella <27522201-citomirella@users.noreply.replit.com>
2023-11-13 17:22:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+# declaração de variáveis 
+nome = "Mirella"
+idade = 46
+altura = 1.55
+peso = 69.0
+
+print("Olá, mundo!")
+
+#imprimir na tela o valor das variáveis
+print(nome)
+print(idade)
+print(altura)
+print(peso)
+
+print("O meu nome é", nome, "e tenho", idade, "anos.")
+
+print(type(nome))
+print(type(idade))
+print(type(altura))
+print(type(peso))
+
+print(2 * '\n')
+print("-------------------------")
+print(2 * '\n')
+
+nome1 = input("Digite o seu nome: ")
+idade1 = int(input("Digite a sua idade: "))
+altura1 = float(input("Digite a sua altura: "))
+print("Seu nome é", nome1, ", você tem", idade1, "anos e", altura1, "de altura.")
